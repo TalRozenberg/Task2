3409b2b72bb4df64c5dac5abb5684ba7f9a13a70
benjamin-ono <benjishnerik12@gmail.com>
2024-04-13 22:00:31 +0300

    .

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 import {db} from './firebase-config';
 console.log(db);
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDs8T5jUuOhlv2X734VjDniS3n-bNcYQ0U",
+    authDomain: "task-2-f4bc3.firebaseapp.com",
+    projectId: "task-2-f4bc3",
+    storageBucket: "task-2-f4bc3.appspot.com",
+    messagingSenderId: "209943179021",
+    appId: "1:209943179021:web:d8f6848a969e52bde54a44"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
